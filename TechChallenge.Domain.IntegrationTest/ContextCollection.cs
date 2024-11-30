1f21be90e2fee28e41cf92e797fd57551635cce2
Clademir Zampieri <59873475+ClademirZampieri@users.noreply.github.com>
2024-11-30 00:45:57 -0300

    Update ContextCollection.cs

--- TechChallenge.Domain.IntegrationTest/ContextCollection.cs
+++ TechChallenge.Domain.IntegrationTest/ContextCollection.cs
@@ -13,10 +13,10 @@ public class ContextFixture : IAsyncLifetime
 {
     public techchallengeDbContext _context { get; private set; }
     private readonly MsSqlContainer _msSqlContainer = new MsSqlBuilder()
-    .WithImage("mcr.microsoft.com/windows/servercore:ltsc2022")
-    .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(1433)) // Verifica a porta 1433
-    .WithStartupTimeout(TimeSpan.FromMinutes(2)) // Aumenta o tempo limite de inicialização
-    .Build();
+        .WithImage("mcr.microsoft.com/mssql/server:2022-CU13-windowsservercore-ltsc2022")
+        .WithWaitStrategy(Wait.ForWindowsContainer().UntilPortIsAvailable(1433))
+        .WithStartupTimeout(TimeSpan.FromMinutes(5)) // Aumenta o timeout
+        .Build();
 
     public async Task InitializeAsync()
     {
